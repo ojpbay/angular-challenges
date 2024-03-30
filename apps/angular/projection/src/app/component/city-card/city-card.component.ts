391d1f3e172604543a3c96b01f334f1e42b67ae8
ojpbay <oli.baylis@live.co.uk>
2024-03-30 09:48:47 +0000

    feat(projection): project image and hook up city display before refactoring list

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,38 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CityStore } from '../../data-access/city.store';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { CardType } from '../../model/card.model';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="cities$ | async"
+      [type]="cardType"
+      customClass="bg-light-blue">
+      <img src="assets/img/city.png" width="200px" />
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, AsyncPipe],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cardType = CardType.CITY;
 
-  ngOnInit(): void {}
+  constructor(
+    private http: FakeHttpService,
+    private store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  get cities$(): Observable<City[]> {
+    return this.store.cities$;
+  }
 }
